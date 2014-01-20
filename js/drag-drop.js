0290b39e5ff2c4588b06543e344a2ac75f40569b
Udit <Thoughtworker@UM9677.local>
2014-01-20 05:10:04 -0800

    location sorting words

--- js/drag-drop.js
+++ js/drag-drop.js
@@ -1,9 +1,21 @@
+var amp = {};
+amp.words = ["hello", "world"]
+amp.locationSortedWords = function(){
+  var words = _.map(amp.words, function(a){ 
+    return {
+      name: a,
+      left_location: $("#"+ a).offset().left
+    };
+  });
+   return _.sortBy(words, function(b){return b.left_location});
+
+}
 $(function(){
   $("#hello").draggable({containment: "#jail"});
   $("#world").draggable({containment: "#jail"});
   $("#container").droppable({
     drop: function(evt, ui){
-      alert("drop");
+      console.log("drop");
     } 
   })
 })
