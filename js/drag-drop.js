f7a243a02a3411d4b757218f238c2c512923b4a5
Udit <Thoughtworker@UM9677.local>
2014-01-21 21:13:56 -0800

    rename amp to emp

--- js/drag-drop.js
+++ js/drag-drop.js
@@ -1,7 +1,7 @@
-var amp = {};
-amp.words = ["hello", "world", "steve"]
-amp.locationSortedWords = function(){
-  var words = _.map(amp.words, function(a){ 
+var emp = {};
+emp.words = ["hello", "world", "steve"]
+emp.locationSortedWords = function(){
+  var words = _.map(emp.words, function(a){ 
     return {
       name: a,
       left_location: $("#"+ a).offset().left
@@ -10,7 +10,7 @@ amp.locationSortedWords = function(){
    return _.sortBy(words, function(b){return b.left_location});
 }
 
-amp.sortedWordsWithRelativeDistances = function(){
+emp.sortedWordsWithRelativeDistances = function(){
   var sortedWords = this.locationSortedWords();
   return _.map(sortedWords, function(word, index, words){
     var firstWordLeftOffset = words[0].left_location;
@@ -21,7 +21,7 @@ amp.sortedWordsWithRelativeDistances = function(){
   });
 }
 
-amp.audio = {
+emp.audio = {
   playSound: function(buffer, time) {
     var source = context.createBufferSource();
     source.buffer = buffer;
@@ -29,41 +29,41 @@ amp.audio = {
     source.noteOn(time);
   }
 };
-amp.buffers = {};
-amp.context = new webkitAudioContext();
-amp.loadSound = function(url, hash_store, key) {
+emp.buffers = {};
+emp.context = new webkitAudioContext();
+emp.loadSound = function(url, hash_store, key) {
   var request = new XMLHttpRequest();
   request.open('GET', url, true);
   request.responseType = 'arraybuffer';
 
   // Decode asynchronously
   request.onload = function() {
-    amp.context.decodeAudioData(request.response, function(buffer) {
+    emp.context.decodeAudioData(request.response, function(buffer) {
       hash_store[key] = buffer;
     }, function(){console.log("erroe happened");});
   }
   request.send();
 }
-amp.playSound = function(bufferName, time) {
-  var source = amp.context.createBufferSource();
-  source.buffer = amp.buffers[bufferName];
-  source.connect(amp.context.destination);
+emp.playSound = function(bufferName, time) {
+  var source = emp.context.createBufferSource();
+  source.buffer = emp.buffers[bufferName];
+  source.connect(emp.context.destination);
   source.noteOn(time);
 } 
 
-amp.playWords = function(){
+emp.playWords = function(){
   _.each(this.sortedWordsWithRelativeDistances(), function(word){
-    amp.playSound(word.name, amp.context.currentTime + word.relative_location/1000)
+    emp.playSound(word.name, emp.context.currentTime + word.relative_location/1000)
   
   });
 
 }
 
 $(function(){
-  _.each(amp.words, function(word){
-    amp.loadSound("http://localhost:3002/"+ word +".wav", amp.buffers,word);
+  _.each(emp.words, function(word){
+    emp.loadSound("http://localhost:3002/"+ word +".wav", emp.buffers,word);
   })
-  _.each(amp.words, function(word){$("#" + word).draggable({containment: "#jail"})});
+  _.each(emp.words, function(word){$("#" + word).draggable({containment: "#jail"})});
   $("#hello").draggable({containment: "#jail"});
   $("#world").draggable({containment: "#jail"});
   $("#container").droppable({
